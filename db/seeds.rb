ca22ad8eadb73308b0af1ef022ea65d03cc43504
Samanthaponce5 <samanthaponce5@gmail.com>
2020-03-14 08:44:56 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+coupon1 = Coupon.create(coupon_code:"1234", store:"store1")
+coupon2 = Coupon.create(coupon_code:"5678", store:"store2")
+coupon3 = Coupon.create(coupon_code:"91011", store:"store3")
+coupon4 = Coupon.create(coupon_code:"121314", store:"store4")
